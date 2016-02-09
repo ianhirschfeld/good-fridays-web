3cc6fae29383e5b86495557e208ce06716c661c1
Ian Hirschfeld <ianh7976@gmail.com>
2016-02-09 00:39:07 -0800

    Adds track source and uploader to payload.

--- server.rb
+++ server.rb
@@ -15,7 +15,9 @@ get '/tracks.json' do
       title: track.title,
       duration: track.duration,
       artwork_url: track.artwork_url.gsub('large', 't500x500'),
-      stream_url: "#{track.stream_url}?client_id=#{ENV['SOUNDCLOUD_CLIENT_ID']}"
+      stream_url: "#{track.stream_url}?client_id=#{ENV['SOUNDCLOUD_CLIENT_ID']}",
+      source: 'SoundCloud',
+      uploader: track.user.username,
     }
   end.to_json
 end
