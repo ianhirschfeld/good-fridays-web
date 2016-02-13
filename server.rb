82a02dcc9becec6a4396ea4ac5a15b2d303faf1c
Ian Hirschfeld <ianh7976@gmail.com>
2016-02-13 11:49:40 -0800

    Temp: Remove soundcloud tracks that don't allow streaming until the app can handle that case.

--- server.rb
+++ server.rb
@@ -10,14 +10,16 @@ get '/tracks.json' do
   playlist = client.get("/playlists/#{ENV['SOUNDCLOUD_PLAYLIST_ID']}")
   tracks = playlist.tracks
   tracks.map do |track|
-    {
-      id: track.id,
-      title: track.title,
-      duration: track.duration,
-      artwork_url: track.artwork_url.gsub('large', 't500x500'),
-      stream_url: "#{track.stream_url}?client_id=#{ENV['SOUNDCLOUD_CLIENT_ID']}",
-      source: 'SoundCloud',
-      uploader: track.user.username,
-    }
-  end.to_json
+    if track.stream_url
+      {
+        id: track.id,
+        title: track.title,
+        duration: track.duration,
+        artwork_url: track.artwork_url.gsub('large', 't500x500'),
+        stream_url: "#{track.stream_url}?client_id=#{ENV['SOUNDCLOUD_CLIENT_ID']}",
+        source: 'SoundCloud',
+        uploader: track.user.username,
+      }
+    end
+  end.compact.to_json
 end
