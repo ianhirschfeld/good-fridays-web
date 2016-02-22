7368604cac348fb928fda8a0d11e380c8a26bf5b
Ian Hirschfeld <ianh7976@gmail.com>
2016-02-21 16:08:41 -0800

    Adds new endpoint for next version of app.

--- server.rb
+++ server.rb
@@ -23,3 +23,22 @@ get '/tracks.json' do
     end
   end.compact.to_json
 end
+
+get '/tracks_v2.json' do
+  client = Soundcloud.new(client_id: ENV['SOUNDCLOUD_CLIENT_ID'])
+  playlist = client.get("/playlists/#{ENV['SOUNDCLOUD_PLAYLIST_ID']}")
+  tracks = playlist.tracks
+  tracks.map do |track|
+    {
+      id: track.id,
+      title: track.title,
+      duration: track.duration,
+      artwork_url: track.artwork_url.gsub('large', 't500x500'),
+      stream_url: "#{track.stream_url}?client_id=#{ENV['SOUNDCLOUD_CLIENT_ID']}",
+      permalink_url: track.permalink_url,
+      source: 'SoundCloud',
+      uploader: track.user.username,
+      streamable: track.streamable,
+    }
+  end.to_json
+end
